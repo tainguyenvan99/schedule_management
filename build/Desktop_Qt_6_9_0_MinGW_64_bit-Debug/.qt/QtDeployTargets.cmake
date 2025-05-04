9a4a3b20e292c24882659dbb1ed9e0d1ca5f7c6c
tainv99.qn <tainv99.qn@gmail.com>
2025-05-04 17:25:55 +0700

    Remove build folder from repo

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_schedule_management_FILE D:/qt_fw/schedule_management/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/schedule_management.exe)
-set(__QT_DEPLOY_TARGET_schedule_management_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_schedule_management_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
